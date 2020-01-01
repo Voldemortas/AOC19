ca64b3b2e5f4a0abd634576e5e80896165cef144
Andrius Simanaitis <eragonas5@gmail.com>
2020-01-01 23:45:19 +0200

    finished day 1

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ADVENT OF CODE 2019
 
 https://adventofcode.com/2019
+
+-   [day1](src/day1) (2/2)
---
+++ src/day1/part2.ts
@@ -0,0 +1,15 @@
+//part 2git
+import readFromFile from '../read'
+const [lines, chars] = readFromFile('input.txt')
+function calcFuel(fuel: number): number {
+    let amount = Math.floor(fuel / 3) - 2
+    if (amount <= 0) {
+        return 0
+    }
+    return amount + calcFuel(amount)
+}
+console.log(
+    lines
+        .map((e) => Math.floor(parseInt(e)))
+        .reduce((a, b) => a + calcFuel(b), 0)
+)
